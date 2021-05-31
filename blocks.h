ca353394a83543f6aa4214c52d4c5d13dd358947
Dominik Marcinowski <dmarcinowski@zoho.com>
2021-05-31 21:02:44 +0200

    Update widgets

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "pacman -Qqu | wc -l", 60, 0},
-	{"墳 ", "printf '%d%%' `pamixer --get-volume`", 5, 10},
-	{" ", "date '+%b %d %H:%M'", 5, 0},
+// Icon  Command   Update Interval   Update Signal
+//{" ", "pacman -Qqu | wc -l", 60, 0},
+  {" ", "echo $(($(apt list --upgradable 2>/dev/null | wc -l)-1))", 60, 0},
+  {"墳 ", "vol get", 5, 10},
+  {" ", "date '+%a %b %d, %H:%M'", 5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
